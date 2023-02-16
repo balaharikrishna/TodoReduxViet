a0d5e91d98a81e694123760a420e2c3ea29143af
Hari Bala <hari.b@Technovert.com>
2023-02-17 00:13:17 +0530

    added dist file

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base :'https://balaharikrishna.github.io/TodoReduxViet/',
+ // base :'https://balaharikrishna.github.io/TodoReduxViet/',
+ base:'http://localhost:4173/TodoReduxViet/',
   plugins: [react()],
 })
 
